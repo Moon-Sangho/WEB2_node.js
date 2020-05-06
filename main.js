93061033d2a41c3c151a0fbf9f2f9ef278f48893
Moon-Sangho <62928788+Moon-Sangho@users.noreply.github.com>
2020-05-06 20:46:47 +0900

    #2. Node.js에서 URL을 통해서 입력된 값을 사용하는 방법
    기존의 파일 로딩 방식은
    response.end(fs.readFileSync(__dirname + url)); 명령어를 통해 정적인 파일들 (1.html, 2.html, 3.html)을 가져오는 방식이었다.
    만약 파일의 갯수가 늘어난다면 이후 유지, 보수 측면에서 비효율적인 방식이다.
    Query string을 이용하여 웹사이트 유지, 보수 측면에서 좀 더 효율적인 방식으로 개편해나갈 예정이다.

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
 // http 모듈을 요청;
 // fs 모듈을 요청;
-// 변수선언. app = http 모듈의 createServer 메서드를 사용하여 서버 객체 생성;
-// 변수선언. _url = url 주소를 요청;
-// 만약 사용자가 요청한 url의 path가 '/' 라면;
-// url = '/index.html' 로 출력;
+// url 모듈을 요청;
 
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
+// 변수선언. app = http 모듈의 createServer 메서드를 사용하여 서버 객체 생성;
+// 변수선언. _url = url의 path를 요청;
+// 변수선언. queryData = url 문자열을 객체로 변환하여 리턴;
+// 만약 사용자가 요청한 url의 path가 '/' 라면;
+// url path = '/index.html' 로 출력;
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    // console.log(queryData) <-활성화 후 주소창에 localhost:3000/?id=HTML 입력 시 cmd창에 객체로 출력됨 -> {id : HTML}
+    if(_url == '/'){
+      _url = '/index.html';
     }
 
     // 만약 사용자가 요청한 url의 path가 '/favicon.ico' 라면;
     // 헤더 정보에 상태코드 404로 응답;
     // 컨텐츠 출력 완료;
 
-    if(url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
       response.writeHead(404);
       response.end();
       return;
     }
 
     // 헤더 정보에 상태코드 200으로 응답;
-    // 동기식으로 파일을 읽음;
+    // queryData 객체 중 id값을 출력;
     // port 3000으로 서버를 실행;
 
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(queryData.id);
 });
 app.listen(3000);
