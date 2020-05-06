36a676f372abc30f83cbb804471d4cfa3fea95ca
Moon-Sangho <62928788+Moon-Sangho@users.noreply.github.com>
2020-05-06 23:05:26 +0900

    #4. 파일을 이용해 본문 구현
    기존의 정적인 웹페이지인 1.html, 2.html, 3.html 에서는 제목과 본문의 내용 차이만 있을 뿐, 기본적인 태그들과 페이지 구성방식이 똑같았다. 이것을 더욱 효율적으로 관리하기 위해, data 폴더 안에 HTML, CSS, JavaScript 별로 각각 본문 내용만 저장하였고, 그것을 main.js 파일에서 불러올 수 있도록 설정하였다.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // http 모듈을 요청;
 // fs 모듈을 요청;
 // url 모듈을 요청;
-
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -12,7 +11,6 @@ var url = require('url');
 // 변수선언. title = queryData 객체의 id값;
 // 만약 사용자가 요청한 url의 path가 '/' 라면;
 // 변수 title = Welcome 으로 출력;
-
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -25,7 +23,6 @@ var app = http.createServer(function(request,response){
     // 만약 사용자가 요청한 url의 path가 '/favicon.ico' 라면;
     // 헤더 정보에 상태코드 404로 응답;
     // 컨텐츠 출력 완료;
-
     if(_url == '/favicon.ico'){
       response.writeHead(404);
       response.end();
@@ -33,33 +30,33 @@ var app = http.createServer(function(request,response){
     }
 
     // 헤더 정보에 상태코드 200으로 응답;
+    // fs 모듈의 메소드인 readFile() 실행; readFile(읽고자 하는 파일 이름, 읽을 때 옵션, 파일이 읽혀진 후 호출될 함수)
     // 변수선언. template = 1.html 내용 복사 후, 페이지별로 바뀌어야할 부분들을 변수로 선언한 뒤, 본문에 삽입시킴;
     response.writeHead(200);
-    var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-      <img src="coding.jpg" width="100%">
-      </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-      </p>
-    </body>
-    </html>
-    `;
+    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
 
-    // 변수 template 출력;
-    response.end(template);
+      // 변수 template 출력;
+      response.end(template);
+    })
 });
 //port 3000으로 서버를 실행;
 app.listen(3000);
