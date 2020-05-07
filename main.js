e46a741f89533700fdf0e11526192afa97897678
Moon-Sangho <62928788+Moon-Sangho@users.noreply.github.com>
2020-05-07 23:13:11 +0900

    #6. 홈페이지 구현
    이전 커밋에서는 메인페이지의 title과 description이 undefined로 출력되었었다.
    따라서 이번 커밋에서 그 부분을 수정하였으며,
    수정 내용 중 if-else문 안에 또 한 번 if-else문이 추가되어, 그것에 대한 주석을 #1과 #2로 구분하였다.

--- main.js
+++ main.js
@@ -9,18 +9,20 @@ var url = require('url');
 // 변수선언. _url = url의 path를 요청;
 // 변수선언. queryData = url 문자열 중 query를 객체로 변환하여 리턴;
 // 변수선언. pathname = url 문자열 중 pathname을 객체로 변환하여 리턴;
-// 변수선언. title = queryData 객체의 id값;
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
 
-    // 만약 사용자가 요청한 url의 pathname이 '/' 라면;
-    // fs 모듈의 메소드인 readFile() 실행; readFile(읽고자 하는 파일 이름, 읽을 때 옵션, 파일이 읽혀진 후 호출될 함수)
-    // 변수선언. template = 1.html 내용 복사 후, 페이지별로 바뀌어야할 부분들을 변수로 선언한 뒤, 본문에 삽입시킴;
+    // # 1. 만약 사용자가 요청한 url의 pathname이 '/' 라면 (*pathname은 첫 슬래시를 포함해서 호스트 이후부터 쿼리 이전까지의 URL의 경로 부분);
+    // # 2. 만약 queryData.id가 undefined라면;
+    // # 2. 변수선언. title = 'Welcome';
+    // # 2. 변수선언. description = 'Hello, Node.js';
+    // # 2. 변수선언. template = 아래 내용;
     if(pathname === '/'){
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+      if(queryData.id === undefined){
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
         var template = `
         <!doctype html>
         <html>
@@ -40,15 +42,47 @@ var app = http.createServer(function(request,response){
         </body>
         </html>
         `;
-        // 헤더 정보에 상태코드 200으로 응답;
-        // 변수 template 출력;
+        // # 2. 헤더 정보에 상태코드 200으로 응답;
+        // # 2. 변수 template 출력;
         response.writeHead(200);
         response.end(template);
-      });
 
-      // 그렇지 않다면;
-      // 헤더 정보에 상태코드 404로 응답;
-      // 'Not found' 문구 출력;
+        // # 2. 그렇지 않다면;
+        // # 2. fs 모듈의 메서드인 readFile() 실행. readFile(읽고자 하는 파일 이름, 읽을 때 옵션, 파일이 읽혀진 후 호출될 함수);
+        // # 2. 변수선언. title = queryData 객체의 id값;
+        // # 2. 변수선언. template = 1.html 내용 복사 후, 페이지별로 바뀌어야할 부분들을 변수로 선언한 뒤, 본문에 삽입시킴;
+      } else {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          // 헤더 정보에 상태코드 200으로 응답;
+          // 변수 template 출력;
+          response.writeHead(200);
+          response.end(template);
+        });
+      }
+
+      // #1. 그렇지 않다면 (만약 사용자가 요청한 url의 pathname이 '/' 아니라면);
+      // #1. 헤더 정보에 상태코드 404로 응답;
+      // #1. 'Not found' 문구 출력;
     } else {
       response.writeHead(404);
       response.end('Not found');
