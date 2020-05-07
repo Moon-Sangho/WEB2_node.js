b1d332db5d569d292e2c39dfe1e19501d92334ee
Moon-Sangho <62928788+Moon-Sangho@users.noreply.github.com>
2020-05-08 01:15:08 +0900

    #7. 글목록 출력하기
    (주석이 빠진 파일이 올라가서 다시 커밋함)
    기존 코드에서는 웹 페이지에 글 목록을 추가하고 싶을 때, 일일이 <ul>태그 안에 <li>태그를 삽입해야 했다.
    이번 커밋에서는 그 문제점을 보완하였다.
    data 폴더 안에 data file을 추가하기만 하면, 웹페이지에 그것이 자동적으로 업데이트가 되도록 설정하였다.
    fs모듈의 readdir 메서드로 data 폴더 안에 있는 파일들의 제목을 읽도록 하였고,
    메서드 안에 반복문을 추가하여 목록의 자동 업데이트가 가능하도록 하였다.

--- main.js
+++ main.js
@@ -9,51 +9,34 @@ var url = require('url');
 // 변수선언. _url = url의 path를 요청;
 // 변수선언. queryData = url 문자열 중 query를 객체로 변환하여 리턴;
 // 변수선언. pathname = url 문자열 중 pathname을 객체로 변환하여 리턴;
+// # 1. 만약 사용자가 요청한 url의 pathname이 '/' 라면 (*pathname은 첫 슬래시를 포함해서 호스트 이후부터 쿼리 이전까지의 URL의 경로 부분);
+// # 2. 만약 queryData.id가 undefined라면;
+// # 2. fs 모듈의 메서드인 readdir() 실행. 같은 위치의 data 폴더 안에 있는 파일들의 제목을 읽음;
+// # 2. 변수선언. title = 'Welcome';
+// # 2. 변수선언. description = 'Hello, Node.js';
+// # 2. 변수선언. list = '<ul>';
+// # 2. 변수선언. i=0;
+// # 2. i 값이 data 폴더 안 파일들의 length보다 작다면;
+// # 2. list = 아래 내용;
+// # 2. i 값에 1을 더해가며 while(반복)문의 조건에 부합할 때까지만 반복;
+// # 2. 변수 list = 기존 list 형식 + </ul>;
+// # 2. 변수선언. template = 아래 내용;
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-
-    // # 1. 만약 사용자가 요청한 url의 pathname이 '/' 라면 (*pathname은 첫 슬래시를 포함해서 호스트 이후부터 쿼리 이전까지의 URL의 경로 부분);
-    // # 2. 만약 queryData.id가 undefined라면;
-    // # 2. 변수선언. title = 'Welcome';
-    // # 2. 변수선언. description = 'Hello, Node.js';
-    // # 2. 변수선언. template = 아래 내용;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        // # 2. 헤더 정보에 상태코드 200으로 응답;
-        // # 2. 변수 template 출력;
-        response.writeHead(200);
-        response.end(template);
-
-        // # 2. 그렇지 않다면;
-        // # 2. fs 모듈의 메서드인 readFile() 실행. readFile(읽고자 하는 파일 이름, 읽을 때 옵션, 파일이 읽혀진 후 호출될 함수);
-        // # 2. 변수선언. title = queryData 객체의 id값;
-        // # 2. 변수선언. template = 1.html 내용 복사 후, 페이지별로 바뀌어야할 부분들을 변수로 선언한 뒤, 본문에 삽입시킴;
-      } else {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var title = queryData.id;
+        fs.readdir('./data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list+'</ul>';
           var template = `
           <!doctype html>
           <html>
@@ -63,20 +46,60 @@ var app = http.createServer(function(request,response){
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
+            ${list}
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
           </html>
           `;
-          // 헤더 정보에 상태코드 200으로 응답;
-          // 변수 template 출력;
+          // # 2. 헤더 정보에 상태코드 200으로 응답;
+          // # 2. 변수 template 출력;
           response.writeHead(200);
           response.end(template);
+        })
+
+        // # 2. 그렇지 않다면 (만약 queryData.id가 undefined이 아니라면);
+        // # 2. fs 모듈의 메서드인 readdir() 실행. 같은 위치의 data 폴더 안에 있는 파일들의 제목을 읽음;
+        // # 2. 변수선언. list = '<ul>';
+        // # 2. 변수선언. i=0;
+        // # 2. i 값이 data 폴더 안 파일들의 length보다 작다면;
+        // # 2. list = 아래 내용;
+        // # 2. i 값에 1을 더해가며 while(반복)문의 조건에 부합할 때까지만 반복;
+        // # 2. 변수 list = 기존 list 형식 + </ul>;
+        // # 2. fs 모듈의 메서드인 readFile() 실행. readFile(읽고자 하는 파일 이름, 읽을 때 옵션, 파일이 읽혀진 후 호출될 함수);
+        // # 2. 변수선언. title = queryData 객체의 id값;
+        // # 2. 변수선언. template = 아래내용;
+      } else {
+        fs.readdir('./data', function(error, filelist){
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list+'</ul>';
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+            // #2. 헤더 정보에 상태코드 200으로 응답;
+            // #2. 변수 template 출력;
+            response.writeHead(200);
+            response.end(template);
+          });
         });
       }
 
