ff64fdb3a9ceb496c0340b3c5dc3dc2b4dc70fb7
Moon-Sangho <62928788+Moon-Sangho@users.noreply.github.com>
2020-05-12 01:07:33 +0900

    #12. 수정할 정보 전송 & 수정된 내용 저장
    사용자가 수정 링크를 클릭했을 때, create 페이지와 마찬가지로 text를 입력할 수 있는 텍스트 상자들이 나오도록 설정하였고,
    기본적으로 그 텍스트 상자들 안에는 기존 내용들이 출력되도록 하였다.
    또 사용자가 내용을 수정하고 submit 할 시, 그 내용들이 웹 페이지에 반영됨과 동시에 data 폴더 안에 저장되도록 설정하였다.

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ var app = http.createServer(function(request,response){
         });
       }
 
-      // # 1. 그렇지 않고 (pathname이 '/'이 아니고) 만약 pathname이 '/create' 라면;
+      // # 1.  pathname이 '/create' 라면;
       // # 1. fs 모듈의 메서드인 readdir() 실행. 같은 위치의 data 폴더 안에 있는 파일들의 제목을 읽음;
       // # 1. 변수선언. title = 'Welcome - create'
       // # 1. 변수선언. template = templateHTML() 함수실행 (method에서 get과 post방식의 차이점 숙지 중요**);
@@ -110,7 +110,7 @@ var app = http.createServer(function(request,response){
         var title = 'Welcome - create';
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
           <p>
             <input type="text" name="title" placeholder="title">
           </p>
@@ -128,15 +128,15 @@ var app = http.createServer(function(request,response){
         response.end(template);
       });
 
-      // #1. 그렇지 않고 pathname이 '/'도 아니고 '/create'도 아니고 '/create_process'라면;
-      // #1. 변수선언. body ='' (String 형식);
-      // #1. data를 받아옴;
-      // #1. body에 받아온 data 내용을 추가;
-      // #1. data 전송 종료;
-      // #1. 변수선언. post = body 내용을 객체화시킴;
-      // #1. 변수선언. title = post방식으로 전송된 data의 제목;
-      // #1. 변수선언. description = post방식으로 전송된 data의 내용;
-      // #1. data 폴더에 받아온 data 내용을 저장하기 위해 fs모듈의 메서드인 writeFile('대상 파일, 내용, 옵션, 콜백함수') 메서드 사용;
+      // # 1. pathname이 '/create_process'라면;
+      // # 1. 변수선언. body ='' (String 형식);
+      // # 1. data를 받아옴;
+      // # 1. body에 받아온 data 내용을 추가;
+      // # 1. data 전송 종료;
+      // # 1. 변수선언. post = body 내용을 객체화시킴;
+      // # 1. 변수선언. title = post방식으로 전송된 data의 제목;
+      // # 1. 변수선언. description = post방식으로 전송된 data의 내용;
+      // # 1. data 폴더에 받아온 data 내용을 저장하기 위해 fs모듈의 메서드인 writeFile('대상 파일, 내용, 옵션, 콜백함수') 메서드 사용;
     } else if (pathname === '/create_process') {
       var body = '';
       request.on('data', function(data){
@@ -154,7 +154,75 @@ var app = http.createServer(function(request,response){
         })
       });
 
-      // # 1. 그렇지 않다면 (pathname이 '/'도 아니고 '/create'도 아니라면);
+      // # 1. pathname이 '/update'라면;
+      // # 1. fs 모듈의 메서드인 readdir() 실행. 같은 위치의 data 폴더 안에 있는 파일들의 제목을 읽음;
+      // # 1. fs 모듈의 메서드인 readFile() 실행. readFile(읽고자 하는 파일 이름, 읽을 때 옵션, 파일이 읽혀진 후 호출될 함수);
+      // # 1. 변수선언. title = queryData 객체의 id값;
+      // # 1. 변수선언. list = templateList() 함수실행;
+      // # 1. 변수선언. template = templateHTML() 함수실행;
+      // # 1. templateHTML() 함수의 'body' 요소;
+      // # 1. templateHTML() 함수의 'control' 요소; ** name=id로 hidden 속성의 텍스트상자를 따로 만든 이유 :
+      // 사용자가 제목을 변경했을 때, 그것을 data폴더 안에서 찾을 수 없으므로, data 폴더 안의 수정하고자 하는 파일의 제목과 같은 id값이 들어있는 텍스트상자가 필요함;
+    } else if (pathname === '/update'){
+      fs.readdir('./data', function(error, filelist){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list,
+            `
+            <form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p>
+              <input type="text" name="title" placeholder="title" value="${title}">
+            </p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          );
+          // # 1. 헤더 정보에 상태코드 200으로 응답;
+          // # 1. 변수 template 출력;
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+
+      // # 1. pathname이 '/update_process'라면;
+      // # 1. 변수선언. body ='' (String 형식);
+      // # 1. data를 받아옴;
+      // # 1. body에 받아온 data 내용을 추가;
+      // # 1. data 전송 종료;
+      // # 1. 변수선언. post = body 내용을 객체화시킴;
+      // # 1. 변수선언. id = post 객체의 id 값;
+      // # 1. 변수선언. title = post방식으로 전송된 data의 제목;
+      // # 1. 변수선언. description = post방식으로 전송된 data의 내용;
+      // # 1. 사용자가 수정하여 제출한 제목, 내용을 반영하기 위해 fs모듈의 메서드인 rename(`기존 파일 이름`,`변경할 파일 이름`, `콜백함수`) 사용;
+      // # 1. data 폴더에 받아온 data 내용을 저장하기 위해 fs모듈의 메서드인 writeFile('대상 파일, 내용, 옵션, 콜백함수') 메서드 사용;
+    } else if(pathname === '/update_process'){
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error) {
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            // # 1. 헤더 정보에 상태코드 302로 응답; 사용자가 submit 클릭 후 그 페이지를 볼 수 있도록 리다이렉션시킴;
+            // # 1. 화면 출력;
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
+          });  
+        });
+      });
+      // # 1. #1번 가정의 모든 경우가 아니라면;
       // # 1. 헤더 정보에 상태코드 404로 응답;
       // # 1. 화면에 'Not found' 출력;
     } else {
