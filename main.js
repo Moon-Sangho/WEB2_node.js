eb4204d3e327f03b4219ab1b7794974ac267993a
Moon-Sangho <62928788+Moon-Sangho@users.noreply.github.com>
2020-05-14 14:17:05 +0900

    Revert "#17. 출력 정보에 대한 보안"
    This reverts commit 14999c0e863b8bdb95bd7d92f20bc75263433c21.

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 // http 모듈을 요청;
 // fs 모듈을 요청;
 // url 모듈을 요청;
-// querystring 모듈을 요청;
-// lib 폴더 내의 template.js 모듈을 요청;
-// path 모듈을 요청;
-// sanitize-html 모듈을 요청;
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
-var sanitizeHtml = require('sanitize-html');
 
 // 변수선언. app = http 모듈의 createServer 메서드를 사용하여 서버 객체 생성;
 // 변수선언. _url = url의 path를 요청;
@@ -61,17 +56,13 @@ var app = http.createServer(function(request,response){
           var filteredId = path.parse(queryData.id).base;
           fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
             var title = queryData.id;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {
-              allowedTags: ['h1']
-            });
             var list = template.list(filelist);
-            var html = template.HTML(sanitizedTitle, list,
-              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+            var html = template.HTML(title, list,
+              `<h2>${title}</h2>${description}`,
               ` <a href="/create">create</a>
-                <a href="/update?id=${sanitizedTitle}">update</a>
+                <a href="/update?id=${title}">update</a>
                 <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizedTitle}">
+                  <input type="hidden" name="id" value="${title}">
                   <input type="submit" value="delete">
                 </form>`
             );
